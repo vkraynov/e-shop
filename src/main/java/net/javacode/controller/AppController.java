fdf73842362ae97b92b09cae367f4c5b99d90609
JavaIMMB <electropo4ta2015@yandex.ru>
2018-09-25 01:01:56 +0300

    add few souts

--- src/main/java/net/javacode/controller/AppController.java
+++ src/main/java/net/javacode/controller/AppController.java
@@ -3,5 +3,7 @@ package net.javacode.controller;
 public class AppController {
     public static void main(String[] args) {
         System.out.println("Ok!");
+        System.out.println("Program starting...");
+        System.out.println("Done!");
     }
 }
